2e551cc5a78dc7e5cfaeba2f57cc138e821c128d
saurav patankar <sauravpatankar167281@gmail.com>
2026-02-24 09:21:04 +0530

    added layout and basic styling for portfolio webbsite

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactBtn').addEventListener('click', function() {
+    alert('Thanks for reaching out! I will get back to you soon.');
+    console.log('Contact button clicked');
+});
+
+// Subtle scroll reveal logic could go here
